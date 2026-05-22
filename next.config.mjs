5491343cbf2e414e0bdb0aa1a71cf4cfac0859cd
jcanoy <jcanoy@techstyle.com>
2026-05-23 07:45:55 +0800

    fix rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
+const isDev = process.env.NODE_ENV === 'development'
+
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://python-6adtmb7uc-jcanoy-dev.vercel.app/api/:path*',
-      },
-    ]
-  },
+  ...(!isDev && { output: 'export' }),
+  ...(isDev && {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'https://python-6adtmb7uc-jcanoy-dev.vercel.app/api/:path*',
+        },
+      ]
+    },
+  }),
 }
 
 export default nextConfig
